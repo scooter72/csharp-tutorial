b62fb893bcfb0be0fa8859dca2528890c79556d0
unknown <eran.hadar@gmail.com>
2020-03-14 17:10:56 +0200

    new tests

--- koans/Strings.cs
+++ koans/Strings.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace koans
 {
@@ -62,6 +63,55 @@ namespace koans
             Assert.AreEqual("Hello World", concat);
         }
 
+
+        [TestMethod]
+        public void test_plus_equals_also_will_leave_the_original_string_unmodified()
+        {
+            var original = "Hello, ";
+            var hi = original;
+            var there = "World";
+            hi += there;
+
+            Assert.AreEqual(original, string.Empty);
+        }
+
+
+        [TestMethod]
+        public void testEscapedString()
+        {
+            var value = "My name is: \"Bond\"";
+
+            Assert.AreEqual(value, string.Empty);
+        }
+
+        [TestMethod]
+        public void any_csharp_expression_may_be_interpolatedestEscapedString()
+        {
+            var @string = $"The square root of 5 is #{Math.Sqrt(5)}";
+            Assert.AreEqual(@string, string.Empty);
+        }
+
+
+        [TestMethod]
+        public void test_double_quoted_strings_interpolate_variables()
+        {
+            var value = 123;
+            var @string = $"The value is {value}";
+
+            Assert.AreEqual(@string, string.Empty);
+        }
+
+        [TestMethod]
+        public void test_plus_equals_will_concatenate_to_the_end_of_a_string()
+        {
+            var hi = "Hello, ";
+            var there = "World";
+            hi += there;
+
+            Assert.AreEqual(hi, string.Empty);
+        }
+
+
         [TestMethod]
         public void TestStringConcatenationLeavesOriginalStringUnModified()
         {
@@ -69,8 +119,7 @@ namespace koans
             string str2 = "World";
             string concat = str1 + str2;
 
-
-
+            Assert.AreEqual(concat, string.Empty);
             Assert.AreEqual(str1, string.Empty);
             Assert.AreEqual(str2, string.Empty);
         }
@@ -79,22 +128,59 @@ namespace koans
         public void TestStringCharIndex()
         {
             string str1 = "abcd";
-            int indexOfb = -1;
+            int indexOfb = str1.IndexOf("b");
 
-
-
-            Assert.AreEqual(1, indexOfb);
+            Assert.AreEqual(indexOfb,  -1);
         }
 
         [TestMethod]
-        public void TestSubString()
+        public void TestGetSubStringFromString()
         {
-            string str1 = "abcd";
-            string subStr = null;
+            var str1 = "abcdef";
+            Assert.AreEqual(str1.Substring(1, 2), string.Empty);
+        }
+
+        [TestMethod]
+        public void TestGetSubstringOverload()
+        {
+            var str1 = "abcdef";
+            Assert.AreEqual(str1.Substring(4), string.Empty);
+        }
+
+        [TestMethod]
+        public void TestSplit()
+        {
+            string[] tokens = "Sausage Egg Cheese".Split(' ');
+
+            Assert.IsTrue(tokens.SequenceEqual(new string[] { string.Empty, string.Empty, string.Empty }));
+        }
+
+        [TestMethod]
+        public void TestSplitDiffernetPattern()
+        {
+            string[] tokens = "Sausage;Egg;Cheese".Split(';');
+
+            Assert.IsTrue(tokens.SequenceEqual(new string[] { string.Empty, string.Empty, string.Empty }));
+        }
+
+        [TestMethod]
+        public void TestJoin()
+        {
+            var words = new string[] { "Now", "is", "the", "time"};
+            string oneLine = String.Join(" ", words);
+
+            Assert.AreEqual(oneLine, string.Empty);
+        }
 
 
+        [TestMethod]
+        public void TestIdenticalStrings ()
+        {
+            var str1 = "Now" ;
+            var str2 = "Now" ;
 
-            Assert.AreEqual("cd", subStr);
+            Assert.IsTrue(str1 != str2);
+            Assert.IsTrue(str1.GetHashCode() != str2.GetHashCode());
         }
     }
 }
