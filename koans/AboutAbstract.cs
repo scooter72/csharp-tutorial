25ae3a9bae5302db851522fbf377cb72fcda1aa1
scooter <eran.hadar@gmail.com>
2020-03-18 22:14:51 +0200

    new Chapter

---
+++ koans/AboutAbstract.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Koans
+{
+    /// <summary>
+    /// Summary description for AboutAbstract
+    /// </summary>
+    [TestClass]
+    public class AboutAbstract
+    {
+        /// <summary>
+        /// Abstract classes have the following features:
+        /// An abstract class cannot be instantiated.
+        /// An abstract class may contain abstract methods and accessors.
+        /// It is not possible to modify an abstract class with the sealed modifier because the two modifiers have opposite meanings.
+        /// The sealed modifier prevents a class from being inherited and the abstract modifier requires a class to be inherited.
+        /// A non-abstract class derived from an abstract class must include actual implementations of all inherited abstract methods and accessors.
+        /// </summary>
+        abstract class Shape
+        {
+            public abstract int GetArea();
+        }
+
+        class Square : Shape
+        {
+            int side;
+
+            public Square(int n) => side = n;
+
+            // GetArea method is required to avoid a compile-time error.
+            public override int GetArea() => side * side;
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var sq = new Square(12);
+            Assert.AreEqual(0, sq.GetArea());
+        }
+    }
+}
---
+++ koans/AboutStatic.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koans
+{
+    [TestClass]
+    public class AboutStatic
+    {
+        [TestMethod]
+        public void TestSetStaticMemberValue()
+        {
+            MyClass myClass1 = new MyClass("A");
+            MyClass myClass2 = new MyClass("B");
+
+            Assert.AreEqual("A", MyClass.sampleString);
+            MyClass.sampleString = "C";
+            Assert.AreEqual("B", MyClass.sampleString);
+        }
+
+
+        class MyClass
+        {
+            internal static string sampleString = "Sample String";
+
+            internal MyClass(string sampleStringValue)
+            {
+                sampleString = sampleStringValue;
+            }
+        }
+    }
+}
