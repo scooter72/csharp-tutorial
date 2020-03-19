49309657c66c0bb67dc086efe5c4380a52867308
scooter <eran.hadar@gmail.com>
2020-03-19 21:21:07 +0200

    new folder training

--- koans/Training/FirstNonConsecutive.cs
+++ koans/Training/FirstNonConsecutive.cs
@@ -3,12 +3,19 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Koans.Training
 {
+    /// <summary>
+    /// Your task is to find the first element of an array that is not consecutive.
+    /// By not consecutive we mean not exactly 1 larger than the previous element of the array.
+    /// E.g.If we have an array [1,2,3,4,6,7,8] then 1 then 2 then 3 then 4 are all consecutive but 6 is not, so that's the first non-consecutive number.
+    /// If the whole array is consecutive then return null2.
+    /// </summary>
     [TestClass]
     public class FirstNonConsecutive
     {
 
         private static object Run(int[] arr)
         {
+            // implement this method
             throw new NotImplementedException();
         }
 
