bf8951592649f8e8aa68885f1cdfa503d77fbde1
scooter <eran.hadar@gmail.com>
2020-03-25 22:40:24 +0200

    add new training remove duplicate wirds

---
+++ koans/Training/RemoveDuplicateWords.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Koans.Training
+{
+    [TestClass]
+    public class RemoveDuplicateWords
+    {
+
+        public static string RemoveDuplicates(string str)
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ExampleTest()
+        {
+            Assert.AreEqual(Solutions.RemoveDuplicateWords.RemoveDuplicates("alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"),
+                "alpha beta gamma delta");
+        }
+
+        protected static Random rnd = new Random();
+        protected static string[] words = "Alfa, Bravo, Charlie, Delta, Echo, Foxtrot, Golf, Hotel, India, Juliett, Kilo, Lima, Mike, November, Oscar, Papa, Quebec, Romeo, Sierra, Tango, Uniform, Victor, Whiskey, X-ray, Yankee, Zulu".ToLower().Split(new string[] { ", " }, StringSplitOptions.None);
+
+        static string GetString(int length = 20)
+        {
+            return string.Join(" ", new string[length].Select(v => words[rnd.Next(0, words.Length)]));
+        }
+
+        [TestMethod]
+        public void RandomTest()
+        {
+            for (int i = 0; i < 100; ++i)
+            {
+                string testCase = GetString(rnd.Next(20, 50));
+                string actual = RemoveDuplicates(testCase);
+                string expected = Solutions.RemoveDuplicateWords.RemoveDuplicates(testCase);
+
+                Assert.AreEqual(actual, expected);
+            }
+        }
+    }
+}
---
+++ koans/Training/Solutions/RemoveDuplicateWords.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koans.Training.Solutions
+{
+    /*
+     Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
+
+    Example:
+
+    Input:
+
+    'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+
+    Output:
+
+    'alpha beta gamma delta'
+     */
+    public class RemoveDuplicateWords
+    {
+        public static string RemoveDuplicates(string str)
+        {
+            string[] tokens = str.Split(' ');
+            string[] temp = new string[tokens.Length];
+            int numOfDistinct = 0;
+
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                var item = tokens[i];
+                var seen = false;
+                
+                for (int j = 0; j < temp.Length; j++)
+                {
+                    if (item.Equals(temp[j]))
+                    {
+                        seen = true;
+                    }
+                }
+
+                if (!seen)
+                {
+                    temp[numOfDistinct++] = item;    
+                }
+
+            }
+
+            string[] distinctWords = new string[numOfDistinct];
+            for (int i = 0; i < distinctWords.Length; i++)
+            {
+                distinctWords[i] = temp[i];
+            }
+
+
+            return string.Join(" ", distinctWords);
+        }
+    }
+}
