2fc4a58106591c712a8b2f6924e6b9c0b40e17eb
scooter <eran.hadar@gmail.com>
2020-04-03 11:33:17 +0300

    new trainning SimleList

---
+++ koans/Training/SimpleList.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Koans.Training
+{
+    /// <summary>
+    /// Implement the IList interface to create a simple, fixed-size list.
+    /// </summary>
+    [TestClass]
+    public class SimpleList : IList
+    {
+
+        //
+        // Summary:
+        //     Gets or sets the element at the specified index.
+        //
+        // Parameters:
+        //   index:
+        //     The zero-based index of the element to get or set.
+        public object this[int index] 
+        { 
+            get => throw new NotImplementedException(); 
+            set => throw new NotImplementedException(); 
+        }
+
+        public int Count => throw new NotImplementedException();
+        
+        public int Add(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void TestAdd()
+        {
+            IList list = new SimpleList();
+            list.Add(1);
+            // to support index access you need to implement
+            Assert.AreEqual(1, list[0]);
+        }
+
+        [TestMethod]
+        public void TestRemove()
+        {
+            IList list = new SimpleList();
+            list.Add(1);
+            list.Remove(1);
+            Assert.AreEqual(0, list.Count);
+        }
+
+        [TestMethod]
+        public void TestRemoveAt()
+        {
+            IList list = new SimpleList();
+            list.Add(1);
+            list.Add(2);
+            list.RemoveAt(0);
+            Assert.AreEqual(2, list[0]);
+        }
+
+        [TestMethod]
+        public void TestInsert()
+        {
+            IList list = new SimpleList();
+            list.Add(1);
+            list.Insert(0, 2);
+            list.RemoveAt(0);
+            Assert.AreEqual(2, list[0]);
+        }
+
+        [TestMethod]
+        public void TestIndexOf()
+        {
+            IList list = new SimpleList();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            Assert.AreEqual(1, list.IndexOf(2));
+        }
+
+        [TestMethod]
+        public void TestCopyTo()
+        {
+            IList list = new SimpleList();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            int[] expected = new int[] { 1, 2, 3 };
+            int[] actual = new int[3];
+            list.CopyTo(actual, 0);
+            Assert.IsTrue(expected.SequenceEqual(actual));
+        }
+
+        [TestMethod]
+        public void TestContains()
+        {
+            IList list = new SimpleList();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            Assert.IsTrue(list.Contains(2), "list does not contain the element: " + 2);
+        }
+
+        [TestMethod]
+        public void TestClear()
+        {
+            IList list = new SimpleList();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Clear();
+            Assert.IsTrue(list.Count == 0, "list shoud be empty");
+        }
+
+        #region igopre
+        public bool IsReadOnly => false;
+
+        public bool IsFixedSize => true;
+
+        public object SyncRoot => this;
+
+        public bool IsSynchronized => false;
+
+        // ignore not tested
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ koans/Training/Solutions/SimpleList.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Koans.Training.Solutions
+{
+    public class SimpleList : IList
+    {
+        private object[] _contents = new object[8];
+        private int _count;
+
+        public SimpleList()
+        {
+            _count = 0;
+        }
+
+        // IList Members
+        public int Add(object value)
+        {
+            if (_count < _contents.Length)
+            {
+                _contents[_count] = value;
+                _count++;
+
+                return (_count - 1);
+            }
+
+            return -1;
+        }
+
+        public void Clear()
+        {
+            _count = 0;
+        }
+
+        public bool Contains(object value)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (_contents[i] == value)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int IndexOf(object value)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (_contents[i] == value)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void Insert(int index, object value)
+        {
+            if ((_count + 1 <= _contents.Length) && (index < Count) && (index >= 0))
+            {
+                _count++;
+
+                for (int i = Count - 1; i > index; i--)
+                {
+                    _contents[i] = _contents[i - 1];
+                }
+                _contents[index] = value;
+            }
+        }
+
+        public bool IsFixedSize
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public void Remove(object value)
+        {
+            RemoveAt(IndexOf(value));
+        }
+
+        public void RemoveAt(int index)
+        {
+            if ((index >= 0) && (index < Count))
+            {
+                for (int i = index; i < Count - 1; i++)
+                {
+                    _contents[i] = _contents[i + 1];
+                }
+                _count--;
+            }
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                return _contents[index];
+            }
+            set
+            {
+                _contents[index] = value;
+            }
+        }
+
+        // ICollection members.
+
+        public void CopyTo(Array array, int index)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                array.SetValue(_contents[i], index++);
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+        }
+
+        public bool IsSynchronized
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        // Return the current instance since the underlying store is not
+        // publicly available.
+        public object SyncRoot
+        {
+            get
+            {
+                return this;
+            }
+        }
+
+        // IEnumerable Members
+
+        public IEnumerator GetEnumerator()
+        {
+            // Refer to the IEnumerator documentation for an example of
+            // implementing an enumerator.
+            throw new NotImplementedException("The method or operation is not implemented.");
+        }
+
+        public void PrintContents()
+        {
+            Console.WriteLine($"List has a capacity of {_contents.Length} and currently has {_count} elements.");
+            Console.Write("List contents:");
+            for (int i = 0; i < Count; i++)
+            {
+                Console.Write($" {_contents[i]}");
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
